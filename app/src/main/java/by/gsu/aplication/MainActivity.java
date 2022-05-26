852559f28fd6356cbc0f1a68ed4d6bc1a8f876c8
Nikitonz <nikitoniy2468@gmail.com>
2022-05-26 17:39:55 +0300

    MainActivity -> создание списков юзеров

--- app/src/main/java/by/gsu/aplication/MainActivity.java
+++ app/src/main/java/by/gsu/aplication/MainActivity.java
@@ -3,12 +3,46 @@ package by.gsu.aplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<String> users = new ArrayList<String>();
+    ArrayList<String> selectedUsers = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+    ListView usersList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Collections.addAll(users, "ФИО1: +375294566623", "ФИО2: +375295886859");
+
+        usersList = findViewById(R.id.usersList);
+
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, users);
+
+        usersList.setAdapter(adapter);
+
+
+        usersList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+            {
+                
+                String user = adapter.getItem(position);
+                if(usersList.isItemChecked(position))
+                    selectedUsers.add(user);
+                else
+                    selectedUsers.remove(user);
+            }
+        });
     }
 }
